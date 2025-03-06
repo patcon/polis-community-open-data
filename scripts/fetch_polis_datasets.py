82513245114d3cc3131ffa7ffd007feaed72bba4
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-03-05 20:56:26 -0500

    Skip conversations larger than 50MB for now.

--- scripts/fetch_polis_datasets.py
+++ scripts/fetch_polis_datasets.py
@@ -4,6 +4,15 @@ from reddwarf.data_loader import Loader
 
 CSV_FILE = "conversations.csv"
 
+# votes.json files are large.
+# > 50MB: Warning to use git-lfs.
+# > 100MB: Failure, can't push.
+SKIP_CONVO_IDS = [
+    "9xnndurbfm", #  56 MB (warning)
+    "5tzfrp5eaa", #  64 MB (warning)
+    "4uf4hkunf3", # 422 MB (failure)
+]
+
 # Load the CSV data
 df = pd.read_csv(CSV_FILE)
 
@@ -15,5 +24,8 @@ def get_id_from_url(convo_url: str) -> str:
 
 for _, row in df.iterrows():
     convo_id = get_id_from_url(row["Conversation URL"])
+    if convo_id in SKIP_CONVO_IDS:
+        print(f"Skipping conversation {convo_id}... ({row['# Voters']} participants)")
+        continue
     print(f"Processing conversation {convo_id}... ({row['# Voters']} participants)")
     Loader(conversation_id=convo_id, output_dir=f"data/{convo_id}")
